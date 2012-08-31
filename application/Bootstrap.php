1954998468dd5aa20bb0888de6091eeab040c571
marcio <marcio@192.168.1.103>
2012-08-31 09:19:47 -0300

    Identar codigo

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -1,12 +1,12 @@
 <?php
 
-class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
-{
-    protected function _initC() {
-        $config = new Zend_Config_Ini(APPLICATION_PATH . "/configs/application.ini",APPLICATION_ENV);
-        Zend_Registry::set("config",$config);
-        
-    }
+    class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
+    {
+        protected function _initC() {
+            $config = new Zend_Config_Ini(APPLICATION_PATH . "/configs/application.ini",APPLICATION_ENV);
+            Zend_Registry::set("config",$config);
 
-}
+        }
+
+    }
 
