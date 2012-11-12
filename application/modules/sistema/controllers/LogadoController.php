86347aeb57ad94ee946e947c6dc2fe892cf9f985
marcio <marciondb@gmail.com>
2012-11-12 09:14:38 -0200

    debug controle de acesso

--- application/modules/sistema/controllers/LogadoController.php
+++ application/modules/sistema/controllers/LogadoController.php
@@ -191,10 +191,10 @@ class Sistema_LogadoController extends Controller_Action
         $arrayIdUsuario = $this->_funcionario->getIdUsuario($listaIdTempFuncionarioEscolhido);   
         
         $this->_usuario_time_visivel->deletar($arrayIdUsuario);        
-        /*$this->_usuario_empresa_visivel->deletar($arrayIdUsuario);
+        $this->_usuario_empresa_visivel->deletar($arrayIdUsuario);
         $this->_usuario_funcionalidade->deletar($arrayIdUsuario);
         $this->_usuario_grupo->deletar($arrayIdUsuario);
-        
+        /*
         if(isset($parametros['arrayIdTempTimeEscolhido']) && ($parametros['arrayIdTempTimeEscolhido']!=","))
         {            
             $teste = $this->_usuario_time_visivel->gravar($arrayIdUsuario, substr($parametros['arrayIdTempTimeEscolhido'],1,-1));
